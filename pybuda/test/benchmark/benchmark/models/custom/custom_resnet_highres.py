aa726f42638c9c1dcef9f9a04e874c35e8ffef0f
Roy DH Lee <hlee@tenstorrent.com>
2024-02-27 14:03:16 +0000

    Update conf cust models and use w1280xh960 res for custom highres resnet
    (cherry picked from commit 2011ec69836565f57c0eb4c1275dcd66bcd5fb03)
    (cherry picked from commit 0875222a2abff5a36311dccd78c466ad11b7e5dd)

--- pybuda/test/benchmark/benchmark/models/custom/custom_resnet_highres.py
+++ pybuda/test/benchmark/benchmark/models/custom/custom_resnet_highres.py
@@ -16,7 +16,7 @@ from ...common import benchmark_model
 def custom_resnet_highres(training: bool, config: str, microbatch: int, devtype: str, arch: str, data_type: str):
 
     # Set up ONNX model path
-    model_path = "third_party/confidential_customer_models/model_0/files/resnet50_w1280h800.onnx"
+    model_path = "third_party/confidential_customer_models/model_0/files/resnet50_fp32_w1280h960.onnx"
     if not os.path.exists(model_path):
         logger.error("Model not found in path \"{0}\"! Exiting...", model_path)
         exit(1)
@@ -32,12 +32,25 @@ def custom_resnet_highres(training: bool, config: str, microbatch: int, devtype:
 
     # Configurations
     compiler_cfg = _get_global_compiler_config()
+    compiler_cfg.enable_t_streaming = True
+    compiler_cfg.enable_auto_transposing_placement = True
     compiler_cfg.balancer_policy = "Ribbon"
 
     # Overrides
     os.environ["PYBUDA_RIBBON2"] = "1"
+    #os.environ["PYBUDA_RIBBON2_DISABLE_CLEANUP_BUF_NOPS"] = "1"
+    #os.environ["PYBUDA_SPARSE_ENABLE_LAYOUT_DATAFLOW"] = "1"
+    #os.environ["PYBUDA_MAXMIZE_SPARSE_UBLOCK"] = "1"
+    #os.environ["PYBUDA_DISABLE_CAP_SPARSE_MM_FIDELITY"] = "1"
+    #os.environ["PYBUDA_RIBBON2_OPTIMIZATION_ITERATIONS"] = "10"
+    #os.environ["PYBUDA_TEMP_ELT_UNARY_ESTIMATES_LEGACY"] = "1"
     os.environ["PYBUDA_TEMP_ENABLE_NEW_FUSED_ESTIMATES"] = "1"
     os.environ["PYBUDA_TEMP_SCALE_SPARSE_ESTIMATE_ARGS"] = "1"
+    os.environ["PYBUDA_DISABLE_DYNAMIC_DRAM"] = "1"
+    #os.environ["TT_BACKEND_OVERLAY_MAX_EXTRA_BLOB_SIZE"] = f"{77*1024}"
+    os.environ["PYBUDA_FORCE_CONV_MULTI_OP_FRACTURE"] = "1"
+    os.environ["PYBUDA_TEMP_ENABLE_NEW_SPARSE_ESTIMATES"] = "1"
+    os.environ["PYBUDA_RIBBON2_CALCULATE_TARGET_CYCLES"] = "1"
 
     models = {"tt" : pybuda_onnx_model}
     dimension = onnx_model.graph.input[0].type.tensor_type.shape
