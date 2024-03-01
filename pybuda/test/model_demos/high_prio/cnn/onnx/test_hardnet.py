efe3fa51eb8f92f4fbd3690007c4cc608dbe5175
kkannan <kkannan@tenstorrent.com>
2024-03-01 09:22:37 +0000

    remove t stream flag
    (cherry picked from commit ee04cb9c91eefec93c28c7942556761d6d460770)

--- pybuda/test/model_demos/high_prio/cnn/onnx/test_hardnet.py
+++ pybuda/test/model_demos/high_prio/cnn/onnx/test_hardnet.py
@@ -17,7 +17,6 @@ def test_hardnet_onnx(variant, test_device):
     # Set PyBuda configuration parameters
     compiler_cfg = pybuda.config._get_global_compiler_config()
     compiler_cfg.balancer_policy = "Ribbon"
-    compiler_cfg.enable_t_streaming = True
     compiler_cfg.default_df_override = pybuda.DataFormat.Float16_b
     os.environ["PYBUDA_RIBBON2"] = "1"
 
