003897683a0e4b287b47fbc7552d1934d22de0b6
Nikola Obradovic <nobradovic@tenstorrent.com>
2024-05-20 09:30:37 +0000

    [Model] Nightly model fixup after recent compiler default setting change.
    (cherry picked from commit 62b53c0aab4c60557fa3390120108ae966601fa2)

--- pybuda/test/model_demos/high_prio/cnn/pytorch/test_yolo_v5.py
+++ pybuda/test/model_demos/high_prio/cnn/pytorch/test_yolo_v5.py
@@ -112,6 +112,11 @@ def generate_model_yoloV5I640_imgcls_torchhub_pytorch(test_device, variant, size
                 compiler_cfg.place_on_new_epoch("conv2d_210.dc.matmul.11")
                 os.environ["PYBUDA_TEMP_BALANCER_DISABLE_TARGET_PROXIMITY"] = "1"
                 os.environ["PYBUDA_TEMP_RIBBON2_LEGACY_UTIL_EVAL"] = "1"
+                # Temp mitigations for net2pipe errors, should be removed.
+                #
+                os.environ["PYBUDA_TEMP_ENABLE_NEW_FUSED_ESTIMATES"] = "0"
+                os.environ["PYBUDA_TEMP_SCALE_SPARSE_ESTIMATE_ARGS"] = "0"
+                os.environ["PYBUDA_TEMP_ENABLE_NEW_SPARSE_ESTIMATES"] = "0"
         if size in ["m"]:
             os.environ["PYBUDA_RIBBON2"] = "1"
             os.environ["PYBUDA_INSERT_SLICE_FOR_CONCAT"] = "1"
