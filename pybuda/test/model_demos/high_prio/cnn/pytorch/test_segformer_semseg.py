ccccbf7b90bedddcbdfa23c04fef6a77d355a5be
kkannan <kkannan@tenstorrent.com>
2024-05-22 16:55:48 +0000

    split segformer tests based on tasks to avoid timeout error
    (cherry picked from commit 2c82fdc156fe06a076e12481c7c7a63f2c5ad1ed)

--- pybuda/test/model_demos/high_prio/cnn/pytorch/test_segformer_semseg.py
+++ pybuda/test/model_demos/high_prio/cnn/pytorch/test_segformer_semseg.py
@@ -5,8 +5,6 @@ from pybuda.verify.config import TestKind
 from transformers import (
     AutoImageProcessor,
     SegformerForSemanticSegmentation,
-    SegformerForImageClassification,
-    SegformerConfig,
 )
 
 import os
@@ -86,90 +84,7 @@ def test_segformer_semantic_segmentation_pytorch(test_device, variant):
     pixel_values = get_sample_data(variant)
 
     # Create PyBuda module from PyTorch model
-    tt_model = pybuda.PyTorchModule(
-        "pt_" + str(variant.split("/")[-1].replace("-", "_")), model
-    )
-
-    # Run inference on Tenstorrent device
-    verify_module(
-        tt_model,
-        input_shapes=[(pixel_values.shape,)],
-        inputs=[(pixel_values,)],
-        verify_cfg=VerifyConfig(
-            arch=test_device.arch,
-            devtype=test_device.devtype,
-            devmode=test_device.devmode,
-            test_kind=TestKind.INFERENCE,
-            verify_pybuda_codegen_vs_framework=True,
-            verify_tvm_compile=True,
-            pcc=pcc_value,
-        ),
-    )
-
-
-variants_img_classification = [
-    "nvidia/mit-b0",
-    "nvidia/mit-b1",
-    "nvidia/mit-b2",
-    "nvidia/mit-b3",
-    "nvidia/mit-b4",
-    "nvidia/mit-b5",
-]
-
-
-@pytest.mark.parametrize("variant", variants_img_classification)
-def test_segformer_image_classification_pytorch(test_device, variant):
-
-    # Set PyBuda configuration parameters
-    compiler_cfg = pybuda.config._get_global_compiler_config()
-    compiler_cfg.balancer_policy = "Ribbon"
-    compiler_cfg.default_df_override = pybuda.DataFormat.Float16_b
-    os.environ["PYBUDA_RIBBON2"] = "1"
-    os.environ["PYBUDA_DISABLE_PADDING_PASS"] = "1"
-    pcc_value = 0.99
-
-    if test_device.arch == pybuda.BackendDevice.Wormhole_B0:
-
-        if variant in [
-            "nvidia/mit-b1",
-            "nvidia/mit-b2",
-            "nvidia/mit-b3",
-            "nvidia/mit-b4",
-            "nvidia/mit-b5",
-        ]:
-            os.environ["PYBUDA_FORCE_CONV_MULTI_OP_FRACTURE"] = "1"
-
-        if (
-            variant == "nvidia/mit-b0"
-            and test_device.devtype == pybuda.BackendType.Silicon
-        ):
-            pcc_value = 0.97
-
-    elif test_device.arch == pybuda.BackendDevice.Grayskull:
-
-        if (
-            variant in ["nvidia/mit-b1"]
-            and test_device.devtype == pybuda.BackendType.Silicon
-        ):
-            pcc_value = 0.97
-
-    # Set model configurations
-    config = SegformerConfig.from_pretrained(variant)
-    config_dict = config.to_dict()
-    config_dict["return_dict"] = False
-    config = SegformerConfig(**config_dict)
-
-    # Load the model from HuggingFace
-    model = SegformerForImageClassification.from_pretrained(variant, config=config)
-    model.eval()
-
-    # Load the sample image
-    pixel_values = get_sample_data(variant)
-
-    # Create PyBuda module from PyTorch model
-    tt_model = pybuda.PyTorchModule(
-        "pt_" + str(variant.split("/")[-1].replace("-", "_")), model
-    )
+    tt_model = pybuda.PyTorchModule("pt_" + str(variant.split("/")[-1].replace("-", "_")), model)
 
     # Run inference on Tenstorrent device
     verify_module(
