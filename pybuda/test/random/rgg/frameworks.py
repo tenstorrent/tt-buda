6bfe1d22f24fd3253cf66a2d5111763cbfa949c9
Vladimir Brkic <vbrkic@tenstorrent.com>
2024-07-29 11:44:45 +0000

    Move FrameworkTestUtils
    Refactor only
    
    Issue #2755
    
    (cherry picked from commit a638b81bbd263af16281a7326b95ad051d8015db)

--- pybuda/test/random/rgg/frameworks.py
+++ pybuda/test/random/rgg/frameworks.py
@@ -6,6 +6,9 @@
 
 from enum import Enum
 
+from typing import Tuple
+from copy import copy
+
 from .base import Framework
 
 from .pybuda.model import PyBudaModelBuilder
@@ -13,6 +16,42 @@ from .pytorch.model import PyTorchModelBuilder
 
 from pybuda.op_repo import pybuda_operator_repository
 from pybuda.op_repo import pytorch_operator_repository
+from pybuda.op_repo import OperatorDefinition
+
+
+class FrameworkTestUtils:
+
+    @classmethod
+    def copy_framework(cls, framework: Framework, skip_operators: Tuple[str] = []) -> Framework:
+        framework0 = framework
+        framework = copy(framework)
+        framework.operator_repository = copy(framework.operator_repository)
+        cls.skip_operators(framework, skip_operators)
+        assert len(framework.operator_repository.operators) + len(skip_operators) == len(framework0.operator_repository.operators), "Operators count should match after skipping operators"
+        return framework
+
+    @classmethod
+    def skip_operators(cls, framework: Framework, skip_operators: Tuple[str] = []) -> None:
+        initial_operator_count = len(framework.operator_repository.operators)
+        framework.operator_repository.operators = [op for op in framework.operator_repository.operators if op.name not in skip_operators]
+        assert len(framework.operator_repository.operators) + len(skip_operators) == initial_operator_count, "Operators count should match after skipping operators"
+
+    @classmethod
+    def allow_operators(cls, framework: Framework, allow_operators: Tuple[str] = []) -> None:
+        framework.operator_repository.operators = [op for op in framework.operator_repository.operators if op.name in allow_operators]
+        assert len(allow_operators) == len(framework.operator_repository.operators), "Operators count should match allowing skipping operators"
+
+    @classmethod
+    def copy_operator(cls, framework: Framework, operator_name: str) -> OperatorDefinition:
+        operators = framework.operator_repository.operators
+
+        i, operator = next(((i, operator) for i, operator in enumerate(operators) if operator.name == operator_name), (None, None))
+        if not operator:
+            return None
+
+        operator = copy(operator)
+        operators[i] = operator
+        return operator
 
 
 class Frameworks(Enum):
