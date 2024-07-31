094af8a4cabc1991acb378abea55519a0428803d
Vladimir Brkic <vbrkic@tenstorrent.com>
2024-07-31 13:12:31 +0000

    Timeout for long verifications
    Issue #2755
    
    (cherry picked from commit 259f7664aff18fd7843431e0c502e0ad744ceb72)

--- pybuda/test/random/rgg/base.py
+++ pybuda/test/random/rgg/base.py
@@ -20,6 +20,7 @@ from test.utils import Timer
 from .datatypes import RandomizerNode, RandomizerGraph, RandomizerParameters, RandomizerConfig, ExecutionContext
 from .datatypes import RandomizerTestContext
 from .utils import StrUtils, GraphUtils
+from .utils import timeout, TimeoutException
 
 
 class GraphBuilder:
@@ -190,6 +191,20 @@ class RandomizerRunner:
         return model
 
     def verify(self, model: PyBudaModule) -> None:
+
+        verification_timeout = self.test_context.randomizer_config.verification_timeout
+
+        try:
+            @timeout(verification_timeout)
+            def verify_model_timeout() -> None:
+                self.verify_model(model)
+
+            verify_model_timeout()
+        except TimeoutException as e:
+            logger.error(f"Module verification takes too long {e}.")
+            raise e
+
+    def verify_model(self, model: PyBudaModule) -> None:
         """
         Verify the model by building it and performing validation via PyBuda.
         The method is usually implemented once per framework.
