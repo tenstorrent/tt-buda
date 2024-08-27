651a3f14f7d8d1cdf0e2297d59675a1751f38ba3
Kamalraj Kannan <kkannan@tenstorrent.com>
2024-08-27 16:52:00 +0000

    Add test for sequence cls task for microsoft/phi-3-mini-4k-instruct -  pytorch(wb0)
    (cherry picked from commit d599ec1031f8c4deee9bd4a857dc4ebe60f51ed4)

--- pybuda/test/model_demos/high_prio/nlp/pytorch/test_phi3.py
+++ pybuda/test/model_demos/high_prio/nlp/pytorch/test_phi3.py
@@ -3,9 +3,8 @@ from pybuda.verify.backend import verify_module
 from pybuda import VerifyConfig
 from pybuda.verify.config import TestKind
 from pybuda._C.backend_api import BackendDevice
-from transformers import Phi3Config, Phi3ForCausalLM, AutoTokenizer, Phi3ForTokenClassification
+from transformers import Phi3Config, Phi3ForCausalLM, AutoTokenizer, Phi3ForTokenClassification, Phi3ForSequenceClassification
 import os
-import torch
 import pytest
 
 # Masked fill kernal produced invalid results in Silicon BackendType
@@ -57,8 +56,8 @@ def test_phi3_causal_lm(test_device, variant):
         truncation=True,
     )
 
-    input_ids = inputs["input_ids"].to(torch.int32)
-    attn_mask = inputs["attention_mask"].to(torch.float32)
+    input_ids = inputs["input_ids"]
+    attn_mask = inputs["attention_mask"]
 
     tt_model = pybuda.PyTorchModule("pt_" + str(variant.split("/")[-1].replace("-", "_")), model)
 
@@ -109,8 +108,52 @@ def test_phi3_token_classification(test_device, variant):
 
     verify_module(
         tt_model,
-        input_shapes=[input_ids.shape],
-        inputs=[input_ids],
+        input_shapes=[(input_ids.shape,)],
+        inputs=[(input_ids,)],
+        verify_cfg=VerifyConfig(
+            arch=test_device.arch,
+            devtype=test_device.devtype,
+            devmode=test_device.devmode,
+            test_kind=TestKind.INFERENCE,
+        ),
+    )
+
+
+@pytest.mark.parametrize("variant", variants)
+def test_phi3_sequence_classification(test_device, variant):
+
+    # Configurations
+    compiler_cfg = pybuda.config._get_global_compiler_config()
+    compiler_cfg.default_df_override = pybuda._C.DataFormat.Float16_b
+    compiler_cfg.balancer_policy = "Ribbon"
+
+    # Phi3Config from pretrained variant, disable return_dict and caching.
+    config = Phi3Config.from_pretrained(variant)
+    config_dict = config.to_dict()
+    config_dict["return_dict"] = False
+    config_dict["use_cache"] = False
+    config_dict["pad_token_id"] = None
+    config = Phi3Config(**config_dict)
+
+    # Load tokenizer and model from HuggingFace
+    tokenizer = AutoTokenizer.from_pretrained(variant, return_tensors="pt", trust_remote_code=True)
+    model = Phi3ForSequenceClassification.from_pretrained(variant, trust_remote_code=True, config=config)
+    model.eval()
+
+    # input_prompt
+    input_prompt = "the movie was great!"
+
+    # Tokenize input
+    inputs = tokenizer(input_prompt, return_tensors="pt")
+
+    input_ids = inputs["input_ids"]
+
+    tt_model = pybuda.PyTorchModule("pt_" + str(variant.split("/")[-1].replace("-", "_")), model)
+
+    verify_module(
+        tt_model,
+        input_shapes=[(input_ids.shape)],
+        inputs=[(input_ids)],
         verify_cfg=VerifyConfig(
             arch=test_device.arch,
             devtype=test_device.devtype,
